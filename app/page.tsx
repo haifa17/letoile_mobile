679d4d816a0beb0e97c7578767e4d3507e0722a6
haifa khiari <haifa.khiari.visioad@gmail.com>
2026-04-16 13:56:56 +0100

    fix song autoplay

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Portability from "@/components/portability";
 import Migration from "@/components/migration";
 import Offers from "@/components/offers";
 import FAQ from "@/components/faq";
+import SoundCloudPlayer from "@/components/SoundCloudPlayer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
+      <SoundCloudPlayer />
       <Hero />
       <Banner />
       <Portability />
